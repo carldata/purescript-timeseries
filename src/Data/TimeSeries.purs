276674cb70dcc2e9adb069c1f28126f225bac545
Krzysztof Langner <krzysztof@carlsolutions.com>
2017-04-03 10:46:42 +0200

    Updated dependencies

--- src/Data/TimeSeries.purs
+++ src/Data/TimeSeries.purs
@@ -1,2 +1,11 @@
 module Data.TimeSeries where
+
+import Prelude
+
   
+-- | Data points is a time indexed value
+data DataPoint a = 
+    DP { dpIndex :: Int          -- ^ Get data point index.
+       , dpValue :: a            -- ^ Get data point value.
+       }
+                 
