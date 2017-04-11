2dcafb7a582913007bb1b3ee5fb1bbc77989290f
Krzysztof Langner <klangner@gmail.com>
2017-04-11 12:04:17 +0200

    Mockup for anomaly detection

---
+++ src/Data/TimeSeries/Anomalies.purs
@@ -0,0 +1,18 @@
+-- | Anomaly detection in Time Series
+-- | Current implementation looks for outliers
+module Data.TimeSeries.Anomaly (learn, predict) where
+
+
+import Data.TimeSeries as TS
+
+-- | Anomaly detection model
+data Model = Model
+
+-- Convert timestamp in seconds into the DateTime
+learn :: ∀ a. TS.Series a -> Model
+learn xs = Model
+
+
+-- | Predict values based on learned model
+predict :: ∀ a. Model -> TS.Series a -> TS.Series a
+predict model xs = xs
--- test/Test/Data/TimeSeries.purs
+++ test/Test/Data/TimeSeries.purs
@@ -35,24 +35,3 @@ testSeries = do
     let slicing2 = TS.slice start20 end4 s1
     assert $ slicing2.values == []
 
-    -- it "return data point value at given index" $ do
-    --     let pos = posixSecondsToUTCTime 2
-    --     TS.valueAt pos sampleSeries `shouldBe` Just 1.2
-
-    -- it "return Nothing value if wrong index" $ do
-    --     let idx = [10, 20, 30]
-    --     let values = [10.0, 12.0, 32.4]
-    --     let pos = posixSecondsToUTCTime 1234
-    --     TS.valueAt pos (TS.tsSeries idx values) `shouldBe` Nothing
-
-    -- it "return subset" $ do
-    --     let xs = TS.tsSeries [1..5] [10.0, 1.2, 32.4, 0.65, 11.0]
-    --     let start = utcFromSeconds 2
-    --     let end = utcFromSeconds 3
-    --     TS.size (TS.slice start end xs) `shouldBe` 2
-
-    -- it "return empty subset" $ do
-    --     let xs = TS.tsSeries [1..5] [10.0, 1.2, 32.4, 0.65, 11.0]
-    --     let start = utcFromSeconds 12
-    --     let end = utcFromSeconds 34
-    --     TS.slice start end xs `shouldBe` TS.emptySeries    
