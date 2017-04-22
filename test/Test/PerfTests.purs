4b45cf81b2e824e02b26c54061445d4f1b8fadef
klangner <klangner@gmail.com>
2017-04-22 11:52:50 +0200

    Added performance test to the zipWith function

--- test/Test/PerfTests.purs
+++ test/Test/PerfTests.purs
@@ -21,13 +21,25 @@ perfTests :: forall eff. Eff (console :: CONSOLE, assert :: ASSERT, exception ::
 perfTests = do
 
     log "\n# Performance tests"
-    log "Load 60k Time Series from the file"
-    csv <- readTextFile UTF8 "testdata/test60k.csv"
-    let xs = IO.fromCsv csv
-    let s1 = fromMaybe TS.empty $ A.index xs 0
-    let s2 = fromMaybe TS.empty $ A.index xs 1
+    
+    log "Performance of 60k file"
+    csv60k <- readTextFile UTF8 "testdata/test60k.csv"
+    let xs1 = IO.fromCsv csv60k
+    let s1 = fromMaybe TS.empty $ A.index xs1 0
+    let s2 = fromMaybe TS.empty $ A.index xs1 1
     t1 <- now
     let s3 = A.filter ((==) false) $ A.zipWith (==) s1.values s2.values             
     t2 <- now
     log $ "(filter <<< zip) 60k points in " <> show (t2-t1) <> " milliseconds."
     assert $ (A.length s1.values) == 59042
+    
+    log "Performance of 10k file"
+    csv10k <- readTextFile UTF8 "testdata/test10k.csv"
+    let xs2 = IO.fromCsv csv10k
+    let s11 = fromMaybe TS.empty $ A.index xs2 0
+    let s12 = fromMaybe TS.empty $ A.index xs2 1
+    t11 <- now
+    let s13 = TS.zipWith (+) s11 s12
+    t12 <- now
+    log $ "zipWith " <> show (TS.length s13) <> " points in " <> show (t12-t11) <> " milliseconds."
+    assert $ (A.length s13.values) == 10000
