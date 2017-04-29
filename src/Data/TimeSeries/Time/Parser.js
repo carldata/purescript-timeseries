2e6e938b08ad29c0c6e2f4f6b833e0167c7031f2
klangner <klangner@gmail.com>
2017-04-29 22:12:38 +0200

    Removed force to UTC with adding Z. This makes too many problem in the different browsers.

--- src/Data/TimeSeries/Time/Parser.js
+++ src/Data/TimeSeries/Time/Parser.js
@@ -1,10 +1,6 @@
 "use strict";
 
 exports.parseISOTimeImpl = function (str) {
-  // Force UTC zone 
-  if(!str.endsWith("Z")){
-    str += "Z";
-  }
   var dt = new Date(str);
   return dt.getTime();
 };
