9f9cb97add6a200f6a3926074551e671e9d4bddb
Krzysztof Langner <klangner@gmail.com>
2017-05-05 14:05:41 +0200

    Implemented find fixed values

--- src/Data/TimeSeries/Analyze.purs
+++ src/Data/TimeSeries/Analyze.purs
@@ -1,23 +1,25 @@
 -- | Analyze points in the Time Series
-module Data.TimeSeries.Analyze (findMissing, findMissing') where
+module Data.TimeSeries.Analyze (findMissing, findDifference) where
 
 
 import Prelude 
 import Data.Array as A
+import Data.Array.Partial as AP
 import Data.Maybe (fromMaybe)
+import Partial.Unsafe (unsafePartial)
 
 import Data.TimeSeries as TS 
 import Data.TimeSeries.Time (Timestamp)
 
 
--- | Find missing points in time series.
+-- | Find missing points in Time Series.
 -- | Points are estimated from provided time resolution
 findMissing :: ∀ a. TS.Series a -> Timestamp -> Array Timestamp
 findMissing xs dt = findMissing' si dt (TS.index xs) []
     where 
         si = fromMaybe 0.0 $ TS.dpIndex <$> TS.head xs
 
--- Helper function operating on Array of Timestamp
+-- Helper function operating on Array of Timestamps
 findMissing' :: Timestamp          -- ^ Previous Timestamp
              -> Timestamp          -- ^ Time delta
              -> Array Timestamp    -- ^ Imput array with timestamp from the series
@@ -32,3 +34,29 @@ findMissing' idx dt xs out =
       x = fromMaybe 0.0 $ A.head xs
       ts = fromMaybe [] $ A.tail xs
       nidx = idx + dt
+
+
+-- | Find all points with different values in 2 Time Series
+-- | Only consider points which are in both Series
+-- | Return array with index entries of different points.
+findDifference :: ∀ a. Ord a => TS.Series a -> TS.Series a -> Array Timestamp
+findDifference xs ys = findDifference' (TS.toDataPoints xs) (TS.toDataPoints ys) []
+
+findDifference' :: ∀ a. Ord a 
+                => Array (TS.DataPoint a)   -- ^ First Series
+                -> Array (TS.DataPoint a)   -- ^ Second series
+                -> Array Timestamp          -- ^ Partial output array. build so far
+                -> Array Timestamp          -- ^ Output array
+findDifference' [] _ out = out
+findDifference' _ [] out = out
+findDifference' xs ys out = 
+  if TS.dpIndex x > TS.dpIndex y then findDifference' xs yt out
+  else if TS.dpIndex x < TS.dpIndex y then findDifference' xt ys out
+  else findDifference' xt ys $ if TS.dpValue x == TS.dpValue y 
+                               then out 
+                               else A.snoc out (TS.dpIndex x)
+    where 
+      x = unsafePartial $ AP.head xs
+      xt = unsafePartial $ AP.tail xs
+      y = unsafePartial $ AP.head ys
+      yt = unsafePartial $ AP.tail ys    
--- test/Test/Data/TimeSeries/Analyze.purs
+++ test/Test/Data/TimeSeries/Analyze.purs
@@ -15,11 +15,19 @@ testAnalyze = do
 
   log "\n# Analyze points in Time Series"
   findMissingTest
+  findDifferenceTest
 
     
 findMissingTest :: ∀ eff. Eff (console :: CONSOLE, assert :: ASSERT, exception :: EXCEPTION  | eff) Unit
 findMissingTest = do
   log "* Find missing points"
   let xs = TS.series [1.0, 2.0, 4.0, 7.0, 8.0] [1.0, 1.1, 0.9, 1.2, 1.0]
-  let ys = TA.findMissing xs 1.0
-  assert $ ys == [3.0, 5.0, 6.0]
+  assert $ TA.findMissing xs 1.0 == [3.0, 5.0, 6.0]
+
+
+findDifferenceTest :: ∀ eff. Eff (console :: CONSOLE, assert :: ASSERT, exception :: EXCEPTION  | eff) Unit
+findDifferenceTest = do
+  log "* Find difference between Time Series"
+  let xs = TS.series [1.0, 2.0, 4.0, 7.0, 8.0] [1, 2, 4, 7, 8]
+  let ys = TS.series [1.0, 2.0, 3.0, 7.0, 8.0] [1, 2, 0, 0, 8]
+  assert $ TA.findDifference xs ys == [7.0]
